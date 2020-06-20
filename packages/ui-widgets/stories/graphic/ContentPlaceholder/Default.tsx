5f988c151e6695538aad3fe38a9cb43e5aca3b8d
CHENG LI <rivneg@outlook.com>
2020-06-20 19:59:35 +1000

    Content placeholder stories (#72)
    Co-authored-by: Cheng Li <cheng.li@myob.com>

---
+++ packages/ui-widgets/stories/graphic/ContentPlaceholder/Default.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ContentPlaceholder, Card } from '../../../src';
+
+export default () => (
+  <div style={{ display: 'flex', justifyContent: 'center', width: 300, background: '#ebeef1', padding: '2.4rem' }}>
+    <Card>
+      <ContentPlaceholder/>
+    </Card>
+  </div>
+);
