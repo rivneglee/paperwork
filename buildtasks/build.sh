0a7bd431333238d46cd8ec6db75c3f455a617ef0
Cheng Li <cheng.li@myob.com>
2020-08-02 22:05:36 +1000

    Update  build task for branching ui

--- buildtasks/build.sh
+++ buildtasks/build.sh
@@ -3,8 +3,12 @@
 rm -rf builds
 mkdir builds
 mkdir builds/paperwork-web
+mkdir builds/paperwork-web-lite
 
 yarn install
 yarn build
 
 cp -r packages/paperwork-web/build/* builds/paperwork-web
+
+cd packages/paperwork-web && yarn build:lite
+cp -r build/* ../../builds/paperwork-web-lite
