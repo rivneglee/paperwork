34b3114ba071bbfbd1f12253eeaf5c7cf47a1ec9
Alex Li <rivneg@outlook.com>
2021-04-11 11:21:55 +1000

    Deploy storybook to OSS

--- buildtasks/build.sh
+++ buildtasks/build.sh
@@ -4,11 +4,13 @@ rm -rf builds
 mkdir builds
 mkdir builds/paperwork-web
 mkdir builds/paperwork-web-lite
+mkdir builds/ui-widgets
 
 yarn install
 yarn build
 
 cp -r packages/paperwork-web/build/* builds/paperwork-web
+cp -r packages/ui-widgets/build/* builds/ui-widgets
 
 cd packages/paperwork-web && yarn build:lite
 cp -r build/* ../../builds/paperwork-web-lite
